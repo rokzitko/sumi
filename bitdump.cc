4972602d1aca685035395aa023e06518665fed98
Rok Zitko <rok.zitko@ijs.si>
2023-09-02 19:23:48 +0200

    code simplification for output

--- bitdump.cc
+++ bitdump.cc
@@ -34,6 +34,7 @@ int main(int argc, char *argv[])
   const bool iw = input.exists("-iw"); // byte swap for binary input (endianness change)
   const uint64_t count { input.exists("-c") ? std::stoul(input.get("-c")) : 0 }; // number of random numbers generated, 0 = infinite
   const OutputType ot { input.exists("-ot") ? std::stoi(input.get("-ot")) : 1 }; // default is bits as integers (ASCII)
+  auto ows = get_ws(ot);
   const Direction od { input.exists("-od") ? std::stoi(input.get("-od")) : 1 }; // default is 1 (lsb to msb)
   const bool ow = input.exists("-ow"); // byte swap for binary output (endianness change)
   verbose = input.exists("-v");
@@ -51,64 +52,41 @@ int main(int argc, char *argv[])
   }
 
   auto gen = boolreader(filename, ws, id, iw);
-  int ndx = 0;
-  std::bitset<8> b8;
-  std::bitset<16> b16;
-  std::bitset<32> b32;
-  std::bitset<64> b64;
+  std::size_t ndx = 0;
+  std::bitset<64> bits;
   std::cout << std::noboolalpha;
   uint64_t i;
   for (i = 0; i < count || count == 0; i++) {
-   try {
-    bool b = (*gen)();
-    switch (ot) {
-    case OutputType::lsb:
-      std::cout << b << std::endl;
-      break;
-    case OutputType::lsb_8:
-      b8[od == Direction::lm ? ndx : 7-ndx] = b;
-      ndx++;
-      if (ndx == 8) {
-        writebs<uint8_t, 8>(b8, ow);
-        ndx = 0;
+    try {
+      bool b = (*gen)();
+      using enum OutputType;
+      switch (ot) {
+      case lsb:
+        std::cout << b << std::endl;
+        break;
+      case lsb_8:
+      case lsb_16:
+      case lsb_32:
+      case lsb_64:
+        bits[od == Direction::lm ? ndx : ows-1-ndx] = b;
+        ndx++;
+        if (ndx == ows) {
+          write(bits, ows, ow);
+          ndx = 0;
+        }
+        break;
+      case none:
+        break;
+      default:
+        throw std::runtime_error("Output type -ot should be 1, 8, 16, 32, 64 or 99.");
       }
+    } catch (const EOF_exception &) {
+      if (verbose)
+        msg << "nr bits read=" << i << std::endl;
       break;
-    case OutputType::lsb_16:
-      b16[od == Direction::lm ? ndx : 15-ndx] = b;
-      ndx++;
-      if (ndx == 16) {
-        writebs<uint16_t, 16>(b16, ow);
-        ndx = 0;
-      }
-      break;
-    case OutputType::lsb_32:
-      b32[od == Direction::lm ? ndx : 31-ndx] = b;
-      ndx++;
-      if (ndx == 32) {
-        writebs<uint32_t, 32>(b32, ow);
-        ndx = 0;
-      }
-      break;
-    case OutputType::lsb_64:
-      b64[od == Direction::lm ? ndx : 63-ndx] = b;
-      ndx++;
-      if (ndx == 64) {
-        writebs<uint64_t, 64>(b64, ow);
-        ndx = 0;
-      }
-      break;
-    case OutputType::none:
-      break;
-    default:
-      throw std::runtime_error("Output type -ot should be 1, 8, 16, 32, 64 or 99.");
+    } catch (const std::exception& e) {
+      std::cerr << "Error: " << e.what() << std::endl;
+      exit(1);
     }
-   } catch (const EOF_exception &) {
-     if (verbose)
-       msg << "nr bits read=" << i << std::endl;
-     break;
-   } catch (const std::exception& e) {
-     std::cerr << "Error: " << e.what() << std::endl;
-     exit(1);
-   }
   }
 }
